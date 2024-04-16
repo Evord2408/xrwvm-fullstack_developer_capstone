38989ca93690260d7523dc6a00a757803bf49baf
Evord2408 <riccie2408>
2024-04-16 16:28:42 -0400

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,7 +6,9 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    
+    # path for registration
     path(route='register', view=views.registration, name='register'),
 
     # path for login
