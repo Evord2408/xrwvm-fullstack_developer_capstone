423d7730c06a67aeb736138491751fccd9892d40
Evord2408 <riccie2408@yahoo.com>
2024-04-21 05:26:53 -0400

    Adding temporary changes to Github

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,6 +2,7 @@ import LoginPanel from "./components/Login/Login"
 import RegisterPanel from "./components/Register/Register";
 import { Routes, Route } from "react-router-dom";
 import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
 
 function App() {
   return (
