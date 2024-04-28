f782c07d0c8992030df35809e74b6e5f4d61b9f1
Evord2408 <riccie2408@yahoo.com>
2024-04-28 05:55:42 -0400

    Adding temporary changes to Github

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,7 +4,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+
     # Other fields as needed
+
     def __str__(self):
         return self.name  # Return the name as the string representation
 
@@ -18,16 +20,20 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
 
-    
     type = models.CharField(
-        max_length=10, 
-        choices=CAR_TYPES, 
-        default='SUV')
-    year = models.IntegerField(default=2023,
+        max_length=10,
+        choices=CAR_TYPES,
+        default='SUV'
+    )
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
-        ])
+        ]
+    )
+
     # Other fields as needed
+
     def __str__(self):
         return self.name  # Return the name as the string representation
